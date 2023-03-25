6842e8db880ef4cf39f121bde2cdfc6f187173fc
Lukas Palionis <lkpalionis@gmail.com>
2023-03-25 22:29:58 +0200

    Init. commit

---
+++ app/src/main/java/com/example/fintechlab2/Utils/TextCounter.java
@@ -0,0 +1,9 @@
+package com.example.fintechlab2.Utils;
+
+public class TextCounter {
+    public static int getCharsCount(String input){
+        return input.length();
+    }
+    public static int getWordsCount(String input){ return input.length();    }
+
+}
